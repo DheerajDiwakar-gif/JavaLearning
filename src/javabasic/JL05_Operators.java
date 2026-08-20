0a2b7ac899871abe4cf99242e54f92cd2b285b43
DheerajDiwakar <diwakardheeraj.dd@gmail.com>
2026-08-20 13:05:37 +0530

    added operators

--- src/javabasic/JL05_Operators.java
+++ src/javabasic/JL05_Operators.java
@@ -34,24 +34,50 @@ class SimpleInterest{
         return si;
     }
 }
+class CompoundInterest{
+    double CI,A;//CI = compound interest and A = Total money you have at the end
+    double cInterest(double p , double r , double t , double n){
+        A = p*Math.pow((1+(r/n)),(n*t));
+        CI = A - p;
+        return A;
+    }
+}
+class Seconds{
+   Seconds(int totalSeconds){
+       int hours = totalSeconds / 3600;
+       int remaining = totalSeconds % 3600;
+       int minutes = remaining / 60;
+       int seconds = remaining % 60;
+       System.out.printf("Time = %02d:%02d:%02d%n",hours, minutes, seconds);
+   }
+}
+
+
 public class JL05_Operators {
     public static void main() {
         Scanner sc = new Scanner(System.in);
-        //Circle
-        System.out.println("Enter Radius :");
-        float r = sc.nextFloat();
-        AreaOfCircle a = new AreaOfCircle(r);
-        //Simple Interest
-        SimpleInterest s = new SimpleInterest();
-        System.out.println("Enter principle :");
-        float p = sc.nextFloat();
-        System.out.println("Enter Rate :");
-        float rate = sc.nextFloat();
-        System.out.println("Enter Time :");
-        float t = sc.nextFloat();
-        System.out.println("SI =" +"\u20B9"+s.interest(p,rate,t)); // unicode of ₹ is "\u20B9"
-        // Compound Interest
-
+//        //Circle
+//        System.out.println("Enter Radius :");
+//        float r = sc.nextFloat();
+//        AreaOfCircle a = new AreaOfCircle(r);
+//        //Simple Interest
+//        SimpleInterest s = new SimpleInterest();
+//        System.out.println("Enter principle :");
+//        float p = sc.nextFloat();
+//        System.out.println("Enter Rate :");
+//        float rate = sc.nextFloat();
+//        System.out.println("Enter Time :");
+//        float t = sc.nextFloat();
+//        System.out.println("SI =" +"\u20B9"+s.interest(p,rate,t)); // unicode of ₹ is "\u20B9"
+//        // Compound Interest
+//        CompoundInterest c =new CompoundInterest();
+//        System.out.println("how many times per year bank adds interest :");
+//        double n = sc.nextDouble();
+//        System.out.printf("Total Amount (A) = \u20B9%.2f%n", c.cInterest(p, rate, t, n));
+//        System.out.printf("Compound Interest (CI) = \u20B9%.2f%n", c.CI);
+        System.out.println("Enter Seconds :");
+        int second = sc.nextInt();
+        Seconds s = new Seconds(second);
         sc.close();
     }
 
