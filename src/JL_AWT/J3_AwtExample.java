33f7ef50498afc8c47c6edaabe238c2180a0f374
DheerajDiwakar <diwakardheeraj.dd@gmail.com>
2026-09-01 11:25:14 +0530

    Added awt 3

--- src/JL_AWT/J3_AwtExample.java
+++ src/JL_AWT/J3_AwtExample.java
@@ -1,4 +1,27 @@
 package JL_AWT;
 
-public class J3_AwtExample {
+import java.awt.*;
+class J3_AwtExample
+{
+    J3_AwtExample()
+    {
+        Frame f = new Frame();  // creating a Frame
+        Label l = new Label("Employee id:");       // creating a Label
+        Button b = new Button("Submit");    // creating a Button
+        TextField t = new TextField("My name is Dheeraj");  // creating a TextField
+        l.setBounds(20, 50, 80, 30);
+        t.setBounds(20, 100, 200, 30);
+        b.setBounds(100, 150, 80, 30);
+        f.add(l);
+        f.add(t);
+        f.add(b);
+        f.setSize(400,300);
+        f.setTitle("Employee info");
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    public static void main(String []args)
+    {
+        J3_AwtExample aw= new J3_AwtExample();
+    }
 }
