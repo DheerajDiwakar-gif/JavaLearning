65c91195bfa87b73b4ad0cc470b381f02a33c2c3
DheerajDiwakar <diwakardheeraj.dd@gmail.com>
2026-09-01 11:24:11 +0530

    Added Awt2

--- src/JL_AWT/J2_AwtExample.java
+++ src/JL_AWT/J2_AwtExample.java
@@ -1,4 +1,19 @@
 package JL_AWT;
 
-public class J2_AwtExample {
+import java.awt.*;
+
+public class J2_AwtExample extends Frame {
+    J2_AwtExample() {
+        Button b = new Button("Click Me!!");
+        b.setBounds(30, 50, 80, 30);
+        add(b);
+        setSize(300, 300);
+        setTitle("This is our basic AWT example");
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        J2_AwtExample f = new J2_AwtExample();
+    }
 }
