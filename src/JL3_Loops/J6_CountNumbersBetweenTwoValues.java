911241ae5b88963a7acc4ca25953ad6038c59b9a
DheerajDiwakar <diwakardheeraj.dd@gmail.com>
2026-09-03 14:40:57 +0530

    Added Counter B/W Number

--- src/JL3_Loops/J6_CountNumbersBetweenTwoValues.java
+++ src/JL3_Loops/J6_CountNumbersBetweenTwoValues.java
@@ -2,7 +2,15 @@ package JL3_Loops;
 import java.util.Scanner;
 public class J6_CountNumbersBetweenTwoValues {
     public static void main(String[] args) {
+        int A,B,j=0;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First no.:");
+        A = sc.nextInt();
+        System.out.println("Enter First no.:");
+        B = sc.nextInt();
+        for(int i = A;i<B;i++){
+             j++;
+        }System.out.printf("Number Between %d and %d is %d !!",A,B,j-1);
 
     }
 }
